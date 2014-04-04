3710b71b74dc57b24418a0aa3c3ca59f4b31cae9
Chris Allen <cehallen@gmail.com>
2014-04-04 15:43:52 -0400

    added unit tests which necessitate making Owner class

--- app/models/building.rb
+++ app/models/building.rb
@@ -1,2 +1,9 @@
 class Building < ActiveRecord::Base
+  validates :street_address, presence: true
+  validates :city, presence: true
+  validates :state, presence: true, inclusion: { in: %w(
+    AL AK AZ AR CA CO CT DE FL GA HI ID IL IN IA KS KY LA 
+    ME MD MA MI MN MS MO MT NE NV NH NJ NM NY NC ND OH OK 
+    OR PA RI SC SD TN TX UT VT VA WA WV WI WY) }
+  validates :postal_code, presence: true, length: { is: 5 }
 end
--- spec/models/building_spec.rb
+++ spec/models/building_spec.rb
@@ -1,5 +1,24 @@
 require 'spec_helper'
 
 describe Building do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'validations' do
+    it { should have_valid(:street_address).when('234 Aardvaark Lane') }
+    it { should_not have_valid(:street_address).when('', nil) }
+
+    it { should have_valid(:city).when('Kookaburra') }
+    it { should_not have_valid(:city).when('', nil) }
+
+    it { should have_valid(:state).when('WA') }
+    it { should_not have_valid(:state).when('', nil, 'XX') }
+
+    it { should have_valid(:postal_code).when('04576') }
+    it { should_not have_valid(:postal_code).when('', nil, 'abc') }
+
+    # it { should have_valid(:building_owner).when(Owner.new) }
+    # it { should_not have_valid(:building_owner).when(nil, '') }
+  end
+
+  # context 'associations' do
+  #   it { should belong_to(:owner)}
+  # end
 end
