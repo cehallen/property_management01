2a6ae53d1dab49f28ee4ba56ebfae3db1477cdbe
Chris Allen <cehallen@gmail.com>
2014-04-04 14:18:42 -0400

    First migration of Building model

---
+++ spec/features/create_building_spec.rb
@@ -0,0 +1,50 @@
+require 'spec_helper'
+
+feature 'create a building', %Q{
+  As a real estate associate
+  I want to record a building
+  So that I can refer back to pertinent information
+} do
+
+# Acceptance Criteria:
+
+# * I must specify a street address, city, state, and postal code
+# * Only US states can be specified
+# * I can optionally specify a description of the building
+# * If I enter all of the required information in the 
+#   required format, the building is recorded.
+# * If I do not specify all of the required information in 
+#   the required formats, the building is not recorded and 
+#   I am presented with errors
+# * Upon successfully creating a building, I am redirected 
+#   so that I can record another building.
+
+  scenario 'with valid attributes' do
+    prev_count = Building.count
+    visit buildings_path
+    click_on 'Add a Building'
+    fill_in 'Street Address', with: '33 Harrison Ave'
+    fill_in 'City', with: 'Boston'
+    fill_in 'State', with: 'MA'
+    fill_in 'Postal Code', with: '02355'
+    click_button 'Add Building'
+    expect(page).to have_content('Building added!')
+    expect(page).to have_content('Add Building')
+    expect(Building.count).to eq(prev_count + 1)
+  end
+
+  scenario 'with invalid attributes'
+
+end
+
+
+
+
+
+
+
+
+
+
+
+
