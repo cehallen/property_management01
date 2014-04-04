5f4b6b0f877d1bd3698aa8b4b8617cd2abee527c
Chris Allen <cehallen@gmail.com>
2014-04-04 16:46:22 -0400

    Finished first iteration of add owner feature spec

--- spec/features/create_owner_spec.rb
+++ spec/features/create_owner_spec.rb
@@ -15,10 +15,30 @@ feature 'create a building', %Q{
 #   and I am redirected to enter another new owner
 
   scenario 'with valid attributes' do
-    visit 
+    prev_count = Owner.count
+    visit owners_path
+    click_on 'Add an Owner'
+
+    fill_in 'First Name', with: 'Bruce'
+    fill_in 'Last Name', with: 'Lee'
+    fill_in 'Email', with: 'blee@gmail.com'
+    fill_in 'Company Name', with: 'Taichi Arts'
+    click_button 'Add Owner'
+
+    expect(page).to have_content('Owner added!')
+    expect(page).to have_content('Add Owner')
+    expect(Owner.count).to eq(prev_count + 1)
   end
 
-  scenario 'with invalid attributes' do
-   
-  end
+  # scenario 'with invalid attributes' do
+  #   prev_count = Owner.count
+  #   visit owners_path
+  #   click_on 'Add an Owner'
+
+  #   click_button 'Add Owner'
+
+  #   expect(page).to have_content("can't be blank")
+  #   expect(page).to have_content('Add Owner')
+  #   expect(Owner.count).to eq(prev_count)
+  # end
 end
